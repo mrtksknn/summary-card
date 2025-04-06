169bd5175458dfefb4ba8ac844db3cb0dc71e614
mrtksknn <mertkeskinmtk6@gmail.com>
2025-04-06 21:50:21 +0300

    Create responsive summary card with html and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'summary-card';
+  data: any[] = [
+    {
+      "category": "Reaction",
+      "score": 80,
+      "icon": "assets/images/icon-reaction.svg"
+    },
+    {
+      "category": "Memory",
+      "score": 92,
+      "icon": "assets/images/icon-memory.svg"
+    },
+    {
+      "category": "Verbal",
+      "score": 61,
+      "icon": "assets/images/icon-verbal.svg"
+    },
+    {
+      "category": "Visual",
+      "score": 72,
+      "icon": "assets/images/icon-visual.svg"
+    }
+  ]
 }
